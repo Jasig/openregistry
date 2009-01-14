12f86e7a651b9b59ccbd5bda244d7a561542908d
Dmitriy Kopylenko <dkopylenko@unicon.net>
2009-01-14 14:30:35 +0000

    NOJIRA: Added initial implementation
    git-svn-id: svn://localhost/openregistry/trunk@16619 1580c273-15eb-1042-8a87-dc5d815c88a0

--- openregistry-repository-jpa-impl/src/main/java/org/openregistry/core/repository/jpa/JpaPersonRepository.java
+++ openregistry-repository-jpa-impl/src/main/java/org/openregistry/core/repository/jpa/JpaPersonRepository.java
@@ -3,20 +3,29 @@ package org.openregistry.core.repository.jpa;
 import org.openregistry.core.repository.PersonRepository;
 import org.openregistry.core.repository.RepositoryAccessException;
 import org.openregistry.core.domain.Person;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  * Person repository implementation built on top of JPA.
  *
  * @since 1.0
- *        TODO: add implementation
+ *        TODO: review the implementation
  */
+@Repository
 public class JpaPersonRepository implements PersonRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+    
 
     /**
      * @see org.openregistry.core.repository.PersonRepository#findByInternalId(Long)
      */
     public Person findByInternalId(Long id) throws RepositoryAccessException {
-        return null;
+        return this.entityManager.find(Person.class, id);
     }
 
     /**
@@ -24,7 +33,7 @@ public class JpaPersonRepository implements PersonRepository {
      * @see org.openregistry.core.repository.PersonRepository#savePerson(org.openregistry.core.domain.Person)
      */
     public Person savePerson(Person person) throws RepositoryAccessException {
-        return null;
+        return this.entityManager.merge(person);
     }
 }
 
