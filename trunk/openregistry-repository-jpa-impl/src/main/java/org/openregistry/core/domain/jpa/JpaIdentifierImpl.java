a1fe98c5b395dc577aea090c883a3484419eec10
Scott Battaglia <scott.battaglia@gmail.com>
2009-01-23 00:57:24 +0000

    NOJIRA
    started to add JPA annotations
    
    git-svn-id: svn://localhost/openregistry@16659 1580c273-15eb-1042-8a87-dc5d815c88a0

--- trunk/openregistry-repository-jpa-impl/src/main/java/org/openregistry/core/domain/jpa/JpaIdentifierImpl.java
+++ trunk/openregistry-repository-jpa-impl/src/main/java/org/openregistry/core/domain/jpa/JpaIdentifierImpl.java
@@ -2,13 +2,25 @@ package org.openregistry.core.domain.jpa;
 
 import org.openregistry.core.domain.Identifier;
 import org.openregistry.core.domain.IdentifierType;
+import org.openregistry.core.domain.Person;
+
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 /**
  * @author Scott Battaglia
  * @version $Revision$ $Date$
  * @since 1.0.0
  */
+@Entity
+@Table(name="prc_identifiers")
 public class JpaIdentifierImpl implements Identifier {
+
+    @ManyToOne
+    @JoinColumn(name="person_id", nullable=false)
+    private Person person;
 
     private JpaIdentifierTypeImpl type;
 
@@ -20,5 +32,9 @@ public class JpaIdentifierImpl implements Identifier {
 
     public String getValue() {
         return this.value;
+    }
+
+    public Person getPerson() {
+        return this.person;
     }
 }
