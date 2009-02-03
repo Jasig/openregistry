dbb4c456485e3cd2db415f3c5588063ae1080415
Nancy Mond <nmond@rutgers.edu>
2009-02-03 14:17:23 +0000

    NOJIRA
    git-svn-id: svn://localhost/openregistry/trunk@16713 1580c273-15eb-1042-8a87-dc5d815c88a0

---
+++ openregistry-service-impl/src/main/java/org/openregistry/service/DefaultServiceExecutionResult.java
@@ -0,0 +1,25 @@
+package org.openregistry.service;
+
+import org.springframework.stereotype.Service;
+import org.openregistry.core.service.ServiceExecutionResult;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Nancy Mond
+ * Date: Feb 2, 2009
+ * Time: 11:46:48 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class DefaultServiceExecutionResult implements ServiceExecutionResult {
+
+    private List errorList = new ArrayList();
+
+    public List getErrorList(){
+        return errorList;
+    }
+
+}
