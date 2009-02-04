2b5bf1effbc3299885c8049523ee9953572c2a5a
Nancy Mond <nmond@rutgers.edu>
2009-02-04 23:02:21 +0000

    NOJIRA
    git-svn-id: svn://localhost/openregistry@16758 1580c273-15eb-1042-8a87-dc5d815c88a0

--- trunk/openregistry-webapp/src/main/java/org/openregistry/core/web/propertyeditors/CountryEditor.java
+++ trunk/openregistry-webapp/src/main/java/org/openregistry/core/web/propertyeditors/CountryEditor.java
@@ -1,12 +1,17 @@
 package org.openregistry.core.web.propertyeditors;
 
 import org.openregistry.core.domain.jpa.JpaCountryImpl;
+import org.openregistry.core.domain.Country;
+import org.openregistry.core.repository.ReferenceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.beans.PropertyEditorSupport;
+import java.util.List;
+import java.util.Iterator;
 
 /**
  * Created by IntelliJ IDEA.
- * User: nmond
+ * User: Nancy Mond
  * Date: Jan 30, 2009
  * Time: 10:34:12 AM
  * To change this template use File | Settings | File Templates.
@@ -15,15 +20,24 @@ public class CountryEditor extends PropertyEditorSupport {
 
     private String format;
 
+    @Autowired(required=true)
+    private ReferenceRepository referenceRepository;
+
     public void setFormat(String format) {
         this.format = format;
     }
 
     public void setAsText(String text) {
-        if (format != null && format.equals("upperCase")) {
-            text = text.toUpperCase();
-        }
 
-        //setValue(type);
+        if (text != null && !(text.trim()!= "")) {
+            List countryList =  referenceRepository.getCountries();
+            Iterator<Country> iterator = countryList.iterator();
+	        while ( iterator.hasNext() ){
+	            Country country = iterator.next();
+                if (country.getCode().equals(text)){
+                    setValue(country);
+                }
+	        }
+        }
     }
 }
