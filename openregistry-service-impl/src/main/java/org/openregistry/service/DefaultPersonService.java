144309b7eb9d5fc595cfadc1e01c623eb1983332
Nancy Mond <nmond@rutgers.edu>
2009-02-23 20:12:31 +0000

    NOJIRA
    git-svn-id: svn://localhost/openregistry/trunk@17135 1580c273-15eb-1042-8a87-dc5d815c88a0

--- openregistry-service-impl/src/main/java/org/openregistry/service/DefaultPersonService.java
+++ openregistry-service-impl/src/main/java/org/openregistry/service/DefaultPersonService.java
@@ -5,6 +5,7 @@ import org.openregistry.core.service.ServiceExecutionResult;
 import org.openregistry.core.domain.Person;
 import org.openregistry.core.domain.Role;
 import org.openregistry.core.repository.PersonRepository;
+import org.openregistry.service.validator.RoleValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.Errors;
@@ -33,6 +34,9 @@ public class DefaultPersonService implements PersonService {
     @Autowired(required = true)
     private AnnotationValidator annotationValidator;
 
+    @Autowired(required=true)
+    private RoleValidator roleValidator;
+
     //The converter is thread safe
     private SpringMessageConverter springMessageConverter = new SpringMessageConverter();
 
@@ -52,40 +56,12 @@ public class DefaultPersonService implements PersonService {
         //Do the actual validation (both using annotations and plain code)
         //and accumulate any validation errors in the created Errors instance
         this.springMessageConverter.convertMessages(annotationValidator.validateObject(role), errors);
-        additionalNonAnnotationBasedValidation(role, errors);
+        roleValidator.validate(role, errors);
         if (!errors.hasErrors()) {
             this.personRepository.savePerson(person);
             executionResult = new DefaultServiceExecutionResult(serviceName, executionDate);
         }
         return executionResult == null ? new DefaultServiceExecutionResult(serviceName, executionDate, errors) : executionResult;
-    }
-
-    /* This should be temporary until we figure out how to validate this using Javalid */
-    private void additionalNonAnnotationBasedValidation(Role role, Errors errors) {
-        validateStartEndDates(role, errors);
-        validateAddresses(role, errors);
-    }
-
-    /*
-     * Role start date must be earlier than role end date
-     */
-    private void validateStartEndDates(Role role, Errors errors) {
-        Date startDate = role.getStart();
-        Date endDate = role.getEnd();
-        if (startDate != null && endDate != null) {
-            int difference = startDate.compareTo(endDate);
-            if (difference >= 0) {
-                errors.rejectValue("start", "startDateEndDateCompareMsg");
-            }
-        }
-    }
-
-    /*
-     *  If any address component is specified then all required address components must
-     *  be specified.
-     */
-    private void validateAddresses(Role role, Errors errors) {
-        //Noop TODO: add implementation
     }
 
 }
