8d794145c7934e5909816874da3abe4fb6c1b19d
Nancy Mond <nmond@rutgers.edu>
2009-02-25 20:02:05 +0000

    NOJIRA
    git-svn-id: svn://localhost/openregistry@17174 1580c273-15eb-1042-8a87-dc5d815c88a0

---
+++ trunk/openregistry-service-impl/src/main/java/org/openregistry/service/validator/PersonValidator.java
@@ -0,0 +1,21 @@
+package org.openregistry.service.validator;
+
+import org.openregistry.core.domain.Person;
+import org.springframework.validation.Errors;
+
+import java.util.Date;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Nancy Mond
+ * Date: Feb 25, 2009
+ * Time: 11:35:51 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PersonValidator {
+
+    public void validate(Person person, Errors errors) {
+        //TODO 
+    }
+
+}
