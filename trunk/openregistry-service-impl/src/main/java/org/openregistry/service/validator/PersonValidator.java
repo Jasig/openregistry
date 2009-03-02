771408d01d5372968fe11b8f112dbf02aec009c2
Nancy Mond <nmond@rutgers.edu>
2009-03-02 21:40:22 +0000

    NOJIRA
    git-svn-id: svn://localhost/openregistry@17251 1580c273-15eb-1042-8a87-dc5d815c88a0

--- trunk/openregistry-service-impl/src/main/java/org/openregistry/service/validator/PersonValidator.java
+++ trunk/openregistry-service-impl/src/main/java/org/openregistry/service/validator/PersonValidator.java
@@ -1,7 +1,9 @@
 package org.openregistry.service.validator;
 
 import org.openregistry.core.domain.Person;
+import org.openregistry.core.domain.Name;
 import org.springframework.validation.Errors;
+import org.springframework.util.StringUtils;
 
 import java.util.Date;
 
@@ -15,7 +17,26 @@ import java.util.Date;
 public class PersonValidator {
 
     public void validate(Person person, Errors errors) {
-        //TODO 
+        //tried to validate using javalid but had problems due to nested name class.
+        validateOfficialName(person, errors);
+    }
+
+    /**
+     * Official Name must have a first and last name.
+     * @param person
+     * @param errors
+     */
+    void validateOfficialName(Person person, Errors errors){
+        Name officialName = person.getOfficialName();
+        String givenName = officialName.getGiven();
+        String familyName = officialName.getFamily();
+
+		if (!StringUtils.hasText(givenName)) {
+		    errors.rejectValue("officialName","firstNameRequiredMsg");
+		}
+        if (!StringUtils.hasText(familyName)) {
+		    errors.rejectValue("officialName","lastNameRequiredMsg");
+		}
     }
 
 }
