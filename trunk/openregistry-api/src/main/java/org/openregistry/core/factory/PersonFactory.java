5a478d14b3ef9b60e7902d22077e171c3e8b9dc8
Nancy Mond <nmond@rutgers.edu>
2009-03-05 17:04:53 +0000

    NOJIRA
    git-svn-id: svn://localhost/openregistry@17292 1580c273-15eb-1042-8a87-dc5d815c88a0

---
+++ trunk/openregistry-api/src/main/java/org/openregistry/core/factory/PersonFactory.java
@@ -0,0 +1,16 @@
+package org.openregistry.core.factory;
+
+import org.openregistry.core.domain.Person;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Nancy Mond
+ * Date: Mar 5, 2009
+ * Time: 11:48:01 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface PersonFactory {
+
+    Person createPerson();
+    
+}
