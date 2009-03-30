f847e855bc766700f450a2d05d6a7b7aad1f9e1e
Nancy Mond <nmond@rutgers.edu>
2009-03-30 12:58:03 +0000

    NOJIRA Changes to use Spring Web Flow and switching to PersonSearch object for Add Person.
    git-svn-id: svn://localhost/openregistry/trunk@17660 1580c273-15eb-1042-8a87-dc5d815c88a0

--- openregistry-api/src/main/java/org/openregistry/core/service/reconciliation/ReconciliationResult.java
+++ openregistry-api/src/main/java/org/openregistry/core/service/reconciliation/ReconciliationResult.java
@@ -1,13 +1,14 @@
 package org.openregistry.core.service.reconciliation;
 
 import java.util.List;
+import java.io.Serializable;
 
 /**
  * @author Scott Battaglia
  * @version $Revision$ $Date$
  * @since 1.0.0
  */
-public interface ReconciliationResult {
+public interface ReconciliationResult extends Serializable {
 
     enum ReconciliationType {EXACT, NONE, MAYBE}
 
