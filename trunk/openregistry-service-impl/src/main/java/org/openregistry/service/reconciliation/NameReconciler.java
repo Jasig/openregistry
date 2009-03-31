5283c698dc57521ad20b28c57bedcafc81e49169
Dave Steiner <steiner@rutgers.edu>
2009-03-31 15:07:46 +0000

    NOJIRA add logging messages for debugging purposes
    git-svn-id: svn://localhost/openregistry@17672 1580c273-15eb-1042-8a87-dc5d815c88a0

--- trunk/openregistry-service-impl/src/main/java/org/openregistry/service/reconciliation/NameReconciler.java
+++ trunk/openregistry-service-impl/src/main/java/org/openregistry/service/reconciliation/NameReconciler.java
@@ -13,6 +13,8 @@ import org.openregistry.core.service.reconciliation.PersonMatch;
 import org.openregistry.core.service.reconciliation.Reconciler;
 import org.openregistry.core.service.reconciliation.ReconciliationResult;
 import org.openregistry.core.service.reconciliation.ReconciliationResult.ReconciliationType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -31,32 +33,42 @@ public final class NameReconciler implements Reconciler {
 	private List<PersonMatch> exactMatches;
 	private List<PersonMatch> partialMatches;
 	
+	protected final Logger logger = LoggerFactory.getLogger(getClass());
+	
 	/* 
 	 * @see org.openregistry.core.service.reconciliation.Reconciler#reconcile(org.openregistry.core.domain.sor.PersonSearch)
 	 */
 	public ReconciliationResult reconcile(PersonSearch personSearch) {
 		Set<? extends Name> names = personSearch.getPerson().getNames();  // TODO deal with multiple names properly
+		logger.info("Reconcile: found " + names.size() + " names for " + personSearch.getPerson().toString());
 		for(Name name: names) {
 			List<Person> matches = this.personRepository.findByFamilyName(name.getFamily());
+			logger.info("Reconcile: found " + matches.size() + " possible matches.");
 			for(Person match: matches) {
 				if (name.getGiven().equals(match.getOfficialName().getGiven())) {  // TODO use all names
+					logger.info("Reconcile: found exact match " + match.getOfficialName().getGiven() + " " + match.getOfficialName().getFamily());
 					exactMatches.add(new PersonMatchImpl(match, 100, Collections.<FieldMatch>emptyList()));
 				} else {
+					logger.info("Reconcile: found partial match " + match.getOfficialName().getGiven() + " " + match.getOfficialName().getFamily());
 					partialMatches.add(new PersonMatchImpl(match, 50, Collections.<FieldMatch>emptyList()));
 				}
 			}
 		}
 		if (exactMatches.isEmpty() && partialMatches.isEmpty()) {
+			logger.info("Reconcile: returning NONE; " + exactMatches.size() + "," + partialMatches.size() + " matches found");
 			return new ReconciliationResultImpl(ReconciliationType.NONE, Collections.<PersonMatch>emptyList());
 		}
 		if (! partialMatches.isEmpty()) {
 		    List<PersonMatch> matches = null;
 			matches.addAll(exactMatches);
 			matches.addAll(partialMatches);
+			logger.info("Reconcile: returning MAYBE; " + exactMatches.size() + "," + partialMatches.size() + " matches found");
 			return new ReconciliationResultImpl(ReconciliationType.MAYBE, matches);
 		} else if (exactMatches.size() > 1) {
+			logger.info("Reconcile: returning MAYBE; " + exactMatches.size() + "," + partialMatches.size() + " matches found");
 			return new ReconciliationResultImpl(ReconciliationType.MAYBE, exactMatches);
 		} else {
+			logger.info("Reconcile: returning EXACT; " + exactMatches.size() + "," + partialMatches.size() + " matches found");
 			return new ReconciliationResultImpl(ReconciliationType.EXACT, exactMatches);
 		}
 	}
