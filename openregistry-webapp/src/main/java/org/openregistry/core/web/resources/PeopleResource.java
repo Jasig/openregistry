d2be7e45f8a95a7982c4c3c1f5334657cfe92110
Dmitriy Kopylenko <dkopylenko@unicon.net>
2009-04-07 20:26:57 +0000

    NOJIRA: Added basic REST support with Jersey
    git-svn-id: svn://localhost/openregistry/trunk@17727 1580c273-15eb-1042-8a87-dc5d815c88a0

--- openregistry-webapp/src/main/java/org/openregistry/core/web/resources/PeopleResource.java
+++ openregistry-webapp/src/main/java/org/openregistry/core/web/resources/PeopleResource.java
@@ -4,7 +4,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.context.annotation.Scope;
 
 import javax.ws.rs.*;
-import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.*;
+import java.net.URI;
 
 /**
  * Root RESTful resource representing people in Open Registry.
@@ -20,6 +21,10 @@ import javax.ws.rs.core.MultivaluedMap;
 @Scope("singleton")
 public class PeopleResource {
 
+    @Context
+    //injected by Jersey
+            UriInfo uriInfo;
+
     @GET
     @Path("{personId}")
     @Produces("text/plain")
@@ -28,9 +33,39 @@ public class PeopleResource {
     }
 
     @POST
-    @Consumes("application/x-www-form-urlencoded")
-    public void postSomeData(MultivaluedMap<String, String> formParams) {
-        System.out.println("GOT HTTP form POSTed: " + formParams);
-    }
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    //resp-type query param is here temporarily, for testing
+    public Response postSomeData(@DefaultValue("201") @QueryParam("resp-type") int respType,
+                                 MultivaluedMap<String, String> formParams) {
 
+        //NOTE: The real handling of the POSTed representation will be done
+        //here by means of marshalling the submitted form data into a Java type
+        //and invoking the internal Open Registry API/APIs (TBD), and based on the outcome
+        //return the appropriate response.
+
+        //The code bolow is just for testing the Jersey framework
+        //and to lay out the foundation for further work.        
+
+        Response response = null;
+        UriBuilder uriBuilder = this.uriInfo.getAbsolutePathBuilder();
+        URI uri = uriBuilder.path("rcpId").path("1234567").build();
+
+        switch (respType) {
+            case 201:
+                response = Response.created(uri).build();
+                break;
+            case 409:
+                //Hack! Just to test. Need more robust way of building a list of links
+                String entityBody = "<link rel=\"person\" href=\"" + uri.toString() + "\"/>" +
+                        "\n<link rel=\"person\" href=\"" + uri.toString() + "\"/>\n";
+                response = Response.status(409).entity(entityBody).build();
+                break;
+            case 307:
+                response = Response.temporaryRedirect(uri).build();
+            default:
+                break;
+        }
+        System.out.println("GOT HTTP form POSTed: " + formParams);
+        return response;
+    }
 }
