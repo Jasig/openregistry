ceffb8ac34d6e44812241cfacfdb807df84acdd4
Dmitriy Kopylenko <dkopylenko@unicon.net>
2009-04-08 18:36:42 +0000

    NOJIRA: Added code for un-marshalling the HTTP form request into 'PersonRequestRepresentation' type
    git-svn-id: svn://localhost/openregistry/trunk@17732 1580c273-15eb-1042-8a87-dc5d815c88a0

--- openregistry-webapp/src/main/java/org/openregistry/core/web/resources/PersonRequestRepresentation.java
+++ openregistry-webapp/src/main/java/org/openregistry/core/web/resources/PersonRequestRepresentation.java
@@ -1,6 +1,8 @@
 package org.openregistry.core.web.resources;
 
+import javax.ws.rs.core.MultivaluedMap;
 import java.io.Serializable;
+import java.util.Map;
 
 /**
  * A simple Java bean encapsulating an incoming request for addition to the registry of
@@ -58,4 +60,51 @@ public class PersonRequestRepresentation implements Serializable {
 
     private String postalCode;
 
+    /**
+     * Un-marshal the incoming form payload
+     *
+     * @param personFormPayload representation of an incoming person
+     * @throws IllegalArgumentException if any of the required fields are missing
+     *                                  or sor IDS are in the numerical form. There are no other validations performed.
+     */
+    public PersonRequestRepresentation(MultivaluedMap<String, String> personFormPayload) throws IllegalArgumentException {
+        if (!checkRequiredData(personFormPayload)) {
+            throw new IllegalArgumentException("The request either does not contain all the required data " +
+                    "or some form fields are misspelled. The submitted data is " + personFormPayload);
+        }
+        try {
+            this.systemOfRecordId = Long.valueOf(personFormPayload.getFirst("sorSourceId"));
+            this.systemOfRecordPersonId = Long.valueOf(personFormPayload.getFirst("sorPersonId"));
+        }
+        catch (NumberFormatException ex) {
+            throw new IllegalArgumentException("'sorSourceId' and 'sorPersonId' fields must be numbers. The submitted " +
+                    "values are: [sorSourceId: " + personFormPayload.getFirst("sorSourceId") + "]; " +
+                    "[sorPersonId: " + personFormPayload.getFirst("sorPersonId") + "]");
+        }
+        this.firstName = personFormPayload.getFirst("firstName");
+        this.lastName = personFormPayload.getFirst("lastName");
+        this.email = personFormPayload.getFirst("email");
+        this.mobilePhoneNumber = personFormPayload.getFirst("mobilePhoneNumber");
+        this.mobileCarrier = personFormPayload.getFirst("mobileCarrier");
+        this.dateOfBirth = personFormPayload.getFirst("dateOfBirth");
+        this.ssn = personFormPayload.getFirst("ssn");
+        this.ruId = personFormPayload.getFirst("ruId");
+        this.netId = personFormPayload.getFirst("netId");
+        this.gender = personFormPayload.getFirst("gender");
+        this.addressLine1 = personFormPayload.getFirst("addressLine1");
+        this.addressLine2 = personFormPayload.getFirst("addressLine2");
+        this.city = personFormPayload.getFirst("city");
+        this.region = personFormPayload.getFirst("region");
+        this.postalCode = personFormPayload.getFirst("postalCode");
+    }
+
+    private boolean checkRequiredData(MultivaluedMap<String, String> formPayload) {
+        return (formPayload.getFirst("sorSourceId") != null
+                && formPayload.getFirst("sorPersonId") != null
+                && formPayload.getFirst("firstName") != null
+                && formPayload.getFirst("lastName") != null
+                && formPayload.getFirst("email") != null
+                && formPayload.getFirst("mobilePhoneNumber") != null
+                && formPayload.getFirst("mobileCarrier") != null);
+    }
 }
