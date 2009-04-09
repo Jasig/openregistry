a756967474de2e61189191a3b635ff14407b4d90
Dmitriy Kopylenko <dkopylenko@unicon.net>
2009-04-09 18:57:14 +0000

    NOJIRA: Added javadoc
    git-svn-id: svn://localhost/openregistry/trunk@17738 1580c273-15eb-1042-8a87-dc5d815c88a0

--- openregistry-webapp/src/main/java/org/openregistry/core/web/resources/PersonIdentifierRepresentation.java
+++ openregistry-webapp/src/main/java/org/openregistry/core/web/resources/PersonIdentifierRepresentation.java
@@ -6,6 +6,13 @@ import javax.xml.bind.annotation.XmlType;
 import javax.xml.bind.annotation.XmlElement;
 
 /**
+ * Simple Java Bean encapsulating the representation of an identifier for a person in the registry
+ * for the purpose of exposing it via RESTful resources.
+ * This is class is marshalled into an XML representation using JAXB.
+ * <p/>
+ * This class is immutable
+ *
+ * @author Dmitriy Kopylenko
  * @since 1.0
  */
 @XmlRootElement
