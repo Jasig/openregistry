b49cd650c03bee31375b48fa918afc4a1eda5cb7
Dmitriy Kopylenko <dkopylenko@unicon.net>
2009-07-13 17:19:41 +0000

    OR-60: Added implementation of activation key validation and invalidation RESTful resources
    git-svn-id: svn://localhost/openregistry/trunk@18298 1580c273-15eb-1042-8a87-dc5d815c88a0

---
+++ openregistry-webapp/src/main/java/org/openregistry/core/web/resources/ActivationKeyProcessorResource.java
@@ -0,0 +1,90 @@
+package org.openregistry.core.web.resources;
+
+import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Scope;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.openregistry.core.service.ActivationService;
+import org.openregistry.core.domain.PersonNotFoundException;
+import org.openregistry.core.domain.ActivationKey;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
+
+import com.sun.jersey.api.NotFoundException;
+
+/**
+ * RESTful resource exposing the activation key processing functions i.e. 'invalidation' and 'validation' of activation keys
+ * for people in Open Registry
+ *
+ * @author Dmitriy Kopylenko
+ * @since 1.0
+ */
+@Component
+@Scope("singleton")
+@Path("/people/{personIdType}/{personId}/activation/{activationKey}")
+public final class ActivationKeyProcessorResource {
+
+    @Autowired
+    private ActivationService activationService;
+
+    @DELETE
+    public Response invalidateActivationKey(@PathParam("personIdType") String personIdType,
+                                            @PathParam("personId") String personId,
+                                            @PathParam("activationKey") String activationKey) {
+        Response response = null;
+        try {
+            this.activationService.invalidateActivationKey(personIdType, personId, activationKey);
+        }
+        catch (IllegalArgumentException e) {
+            throw new NotFoundException(
+                    String.format("The activation key [%s] does not exist", activationKey));
+        }
+        catch (PersonNotFoundException e) {
+            throw new NotFoundException(
+                    String.format("The person resource identified by /people/%s/%s URI does not exist",
+                            personIdType, personId));
+        }
+        catch (IllegalStateException e) {
+            response = Response.status(409).entity(String.format("The activation key [%s] has expired", activationKey))
+                    .type(MediaType.TEXT_PLAIN).build();
+        }
+        //If response is null, that means HTTP 204
+        return response;
+    }
+
+    @GET
+    public Response validateActivationKey(@PathParam("personIdType") String personIdType,
+                                          @PathParam("personId") String personId,
+                                          @PathParam("activationKey") String activationKey) {
+        Response response = null;
+        ActivationKey ak = null;
+        try {
+            ak = this.activationService.getActivationKey(personIdType, personId, activationKey);
+            if(ak.isNotYetValid()) {
+                //HTTP 409
+                response = Response.status(409).entity(String.format("The activation key [%s] is not yet valid for use", activationKey))
+                    .type(MediaType.TEXT_PLAIN).build();
+            }
+            else if(ak.isExpired()) {
+                //HTTP 410
+                response = Response.status(410).entity(String.format("The activation key [%s] has expired", activationKey))
+                    .type(MediaType.TEXT_PLAIN).build();
+            }
+        }
+        catch(PersonNotFoundException e) {
+            throw new NotFoundException(
+                    String.format("The person resource identified by /people/%s/%s URI does not exist",
+                            personIdType, personId));    
+        }
+        catch (IllegalArgumentException e) {
+            throw new NotFoundException(
+                    String.format("The activation key [%s] does not exist", activationKey));
+        }
+        //If response is null, that means HTTP 204
+        return response;
+    }
+}
