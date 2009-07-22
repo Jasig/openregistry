703287095bf6599623ab553614ad33dc960d3e94
Scott Battaglia <scott.battaglia@gmail.com>
2009-07-22 19:11:32 +0000

    NOJIRA started to implement retrieving permissions
    git-svn-id: svn://localhost/openregistry/trunk@18343 1580c273-15eb-1042-8a87-dc5d815c88a0

--- openregistry-repository-jpa-impl/src/main/java/org/openregistry/core/repository/jpa/JpaPermissionRepositoryImpl.java
+++ openregistry-repository-jpa-impl/src/main/java/org/openregistry/core/repository/jpa/JpaPermissionRepositoryImpl.java
@@ -3,6 +3,8 @@ package org.openregistry.core.repository.jpa;
 import org.openregistry.core.domain.Person;
 import org.openregistry.security.PermissionRepository;
 import org.openregistry.security.Privilege;
+import org.openregistry.security.PrivilegeSet;
+import org.openregistry.security.Subject;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -24,22 +26,45 @@ public final class JpaPermissionRepositoryImpl implements PermissionRepository {
     private EntityManager entityManager;
 
     public List<Privilege> getPrivilegesFor(final Privilege.PermissionType userType) {
-        return (List<Privilege>) this.entityManager.createQuery("select r from rule r where r.permissionType = :permissionType").setParameter("permissionType", userType).getResultList();
+        return (List<Privilege>) this.entityManager.createQuery("select r from privilege r where r.permissionType = :permissionType").setParameter("permissionType", userType).getResultList();
     }
 
     public List<Privilege> getPrivilegesForUser(String username, Person person) {
-        final List<Privilege> permissionsForUserOrEveryone = (List<Privilege>) this.entityManager.createQuery("select r from rule r where (r.value = :userName and r.permissionType = :userPermissionType) or r.permissionType = :authenticatedPermissionType or r.permissionType = :anonymousPermissionType").setParameter("userName", username)
+        final List<Privilege> permissions = new ArrayList<Privilege>();
+
+        final List<Privilege> permissionsForUserOrEveryone = (List<Privilege>) this.entityManager.createQuery("select r from privilege r where (r.value = :userName and r.permissionType = :userPermissionType) or r.permissionType = :authenticatedPermissionType or r.permissionType = :anonymousPermissionType").setParameter("userName", username)
+                .setParameter("userPermissionType", Privilege.PermissionType.USER)
+                .setParameter("authenticatedPermissionType", Privilege.PermissionType.AUTHENTICATED)
+                .setParameter("anonymousPermissionType", Privilege.PermissionType.EVERYONE)
+                .getResultList();
+
+        final List<PrivilegeSet> privilegeSets = (List<PrivilegeSet>) this.entityManager.createQuery("select p from privilegeSet p where  (p.value = :userName and p.permissionType = :userPermissionType) or p.permissionType = :authenticatedPermissionType or p.permissionType = :anonymousPermissionType").setParameter("userName", username)
                 .setParameter("userPermissionType", Privilege.PermissionType.USER)
                 .setParameter("authenticatedPermissionType", Privilege.PermissionType.AUTHENTICATED)
                 .setParameter("anonymousPermissionType", Privilege.PermissionType.EVERYONE)
                 .getResultList();
 
         // TODO retrieve these expressions and parse them
-        final List<Privilege> permissionsByExpression = new ArrayList<Privilege>();
+        if (person != null) {
+            final List<Privilege> privilegesByExpression = (List<Privilege>) this.entityManager.createQuery("select p from privilege p where p.permissionType = :permissionType").setParameter("permissionType", Subject.PermissionType.EXPRESSION).getResultList();
+            final List<PrivilegeSet> privilegeSetsByExpression = (List<PrivilegeSet>) this.entityManager.createQuery("select p from privilegeSet p where p.permissionType = :permissionType").setParameter("permissionType", Subject.PermissionType.EXPRESSION).getResultList();
 
-        final List<Privilege> permissions = new ArrayList<Privilege>();
-        permissions.addAll(permissionsByExpression);
+            for (final Privilege p : privilegesByExpression) {
+                // TODO determine if we need to add the privilege
+            }
+
+            for (final PrivilegeSet ps : privilegeSetsByExpression) {
+                // TODO determine if we need to add all the privileges
+            }
+
+
+        }
+
         permissions.addAll(permissionsForUserOrEveryone);
+
+        for (final PrivilegeSet p : privilegeSets) {
+            permissions.addAll(p.getPrivileges());
+        }
 
         return permissions;
     }
