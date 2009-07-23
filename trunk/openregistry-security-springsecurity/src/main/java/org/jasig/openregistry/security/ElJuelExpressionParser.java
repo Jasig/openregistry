443cc5ab382f9a2ada42319763d9b5eedb55475a
Scott Battaglia <scott.battaglia@gmail.com>
2009-07-23 15:43:15 +0000

    NOJIRA added expression parsing
    git-svn-id: svn://localhost/openregistry@18352 1580c273-15eb-1042-8a87-dc5d815c88a0

---
+++ trunk/openregistry-security-springsecurity/src/main/java/org/jasig/openregistry/security/ElJuelExpressionParser.java
@@ -0,0 +1,30 @@
+package org.jasig.openregistry.security;
+
+import org.openregistry.security.ExpressionParser;
+import org.openregistry.core.domain.Person;
+import org.springframework.stereotype.Component;
+import de.odysseus.el.util.SimpleContext;
+import de.odysseus.el.ExpressionFactoryImpl;
+
+import javax.el.ValueExpression;
+import javax.el.ExpressionFactory;
+
+/**
+ * Implementation of the ExpressionParser interface that relies on Unified EL and JUEL.
+ *
+ * @version $Revision$ $Date$
+ * @since 1.0.0
+ */
+@Component
+public final class ElJuelExpressionParser implements ExpressionParser {
+
+    private final ExpressionFactory expressionFactory = new ExpressionFactoryImpl();
+
+    public boolean matches(final Person person, final String expression) {
+        final SimpleContext context = new SimpleContext();
+        context.setVariable("person", this.expressionFactory.createValueExpression(person, Person.class));
+
+        final ValueExpression valueExpression = this.expressionFactory.createValueExpression(context, expression, boolean.class);
+        return (Boolean) valueExpression.getValue(context);
+    }
+}
