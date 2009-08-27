b77b826aa5c86e66c18c99c2c57b315568d5d0fc
Scott Battaglia <scott.battaglia@gmail.com>
2009-08-27 03:52:42 +0000

    NOJIRA
    more test cases and fixed bug with TemporalType for ActivationKeys
    
    git-svn-id: svn://localhost/openregistry/trunk@18475 1580c273-15eb-1042-8a87-dc5d815c88a0

--- openregistry-repository-jpa-impl/src/test/java/org/openregistry/core/service/DefaultActivationServiceIntegrationTests.java
+++ openregistry-repository-jpa-impl/src/test/java/org/openregistry/core/service/DefaultActivationServiceIntegrationTests.java
@@ -29,6 +29,12 @@ import java.util.Date;
 @ContextConfiguration(locations = {"classpath:test-activationServices-context.xml"})
 public final class DefaultActivationServiceIntegrationTests extends AbstractTransactionalJUnit4SpringContextTests {
 
+    private static final String IDENTIFIER_TYPE ="NetId";
+
+    private static final String IDENTIFIER_VALUE ="test";
+
+    private static final String LOCK_VALUE = "LOCK";
+
     @Autowired
     private ActivationService activationService;
 
@@ -48,9 +54,9 @@ public final class DefaultActivationServiceIntegrationTests extends AbstractTran
         final Person person = new JpaPersonImpl();
         person.setDateOfBirth(new Date());
         person.setGender("M");
-        this.simpleJdbcTemplate.update("insert into prd_identifier_types(identifier_t, name) values(null, 'NetId')");
-        final IdentifierType identifierType = this.referenceRepository.findIdentifierType("NetId");
-        final Identifier identifier = person.addIdentifier(identifierType, "test");
+        this.simpleJdbcTemplate.update("insert into prd_identifier_types(identifier_t, name) values(null, '" + IDENTIFIER_TYPE + "')");
+        final IdentifierType identifierType = this.referenceRepository.findIdentifierType(IDENTIFIER_TYPE);
+        final Identifier identifier = person.addIdentifier(identifierType, IDENTIFIER_VALUE);
         identifier.setDeleted(false);
         identifier.setPrimary(true);
         final Name name = person.addName();
@@ -74,6 +80,11 @@ public final class DefaultActivationServiceIntegrationTests extends AbstractTran
         this.entityManager.flush();
         final String newActivationKeyString = this.simpleJdbcTemplate.queryForObject("select activation_key from prc_persons where id = ?", String.class, person.getId());
         assertEquals(newActivationKey.asString(), newActivationKeyString);
+        final Date endDate = this.simpleJdbcTemplate.queryForObject("select act_key_end_date from prc_persons where id = ?", Date.class, person.getId());
+        final Date startDate = this.simpleJdbcTemplate.queryForObject("select act_key_start_date from prc_persons where id = ?", Date.class, person.getId());
+        assertEquals(newActivationKey.getEnd(), endDate);
+        assertEquals(newActivationKey.getStart(), startDate);
+        assertEquals(1,countRowsInTable("prc_persons"));
     }
 
 
@@ -83,25 +94,44 @@ public final class DefaultActivationServiceIntegrationTests extends AbstractTran
         assertEquals(1,countRowsInTable("prc_persons"));
         final String oldActivationKeyString = this.simpleJdbcTemplate.queryForObject("select activation_key from prc_persons where id = ?", String.class, person.getId());
         assertEquals(currentActivationKey.asString(), oldActivationKeyString);
-        final ActivationKey newActivationKey = this.activationService.generateActivationKey("NetId", "test");
-        // TODO: figure out why we need to flush here.  Is it necessary?
+        final ActivationKey newActivationKey = this.activationService.generateActivationKey(IDENTIFIER_TYPE, IDENTIFIER_VALUE);
         this.entityManager.flush();
         final String newActivationKeyString = this.simpleJdbcTemplate.queryForObject("select activation_key from prc_persons where id = ?", String.class, person.getId());
         assertEquals(newActivationKey.asString(), newActivationKeyString);
+        final Date endDate = this.simpleJdbcTemplate.queryForObject("select act_key_end_date from prc_persons where id = ?", Date.class, person.getId());
+        final Date startDate = this.simpleJdbcTemplate.queryForObject("select act_key_start_date from prc_persons where id = ?", Date.class, person.getId());
+        assertEquals(newActivationKey.getEnd(), endDate);
+        assertEquals(newActivationKey.getStart(), startDate);
+        assertEquals(1,countRowsInTable("prc_persons"));
     }
 
 
-    /*
+
     @Test
     public void testVerifyActivationKeyWithPerson() {
-
+        final ActivationKey activationKey = this.activationService.getActivationKey(person, person.getCurrentActivationKey().asString(), LOCK_VALUE);
+        assertEquals(person.getCurrentActivationKey().asString(), activationKey.asString());
+        this.entityManager.flush();
+        final String lockValue = this.simpleJdbcTemplate.queryForObject("select act_key_lock from prc_persons where id = ?", String.class, this.person.getId());
+        final Date lockDate = this.simpleJdbcTemplate.queryForObject("select act_key_lock_expiration from prc_persons where id = ?", Date.class, this.person.getId());
+        assertEquals(LOCK_VALUE, lockValue);
+        // TODO how to test if lock was set correctly
+        assertEquals(1,countRowsInTable("prc_persons"));
     }
+
 
     @Test
     public void testVerifyActivationKeyWithIdentifiers() {
-
+        final ActivationKey activationKey = this.activationService.getActivationKey(IDENTIFIER_TYPE, IDENTIFIER_VALUE, person.getCurrentActivationKey().asString(), LOCK_VALUE);
+        assertEquals(person.getCurrentActivationKey().asString(), activationKey.asString());
+        this.entityManager.flush();
+        final String lockValue = this.simpleJdbcTemplate.queryForObject("select act_key_lock from prc_persons where id = ?", String.class, this.person.getId());
+        final Date lockDate = this.simpleJdbcTemplate.queryForObject("select act_key_lock_expiration from prc_persons where id = ?", Date.class, this.person.getId());
+        assertEquals(LOCK_VALUE, lockValue);
+        // TODO how to test if lock was set correctly
+        assertEquals(1,countRowsInTable("prc_persons"));
     }
-
+    /*
     @Test
     public void testInvalidateActivationKeyWithPerson() {
 
