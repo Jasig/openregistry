98cc57729d99dc4762027769d8db62c26b085301
Scott Battaglia <scott.battaglia@gmail.com>
2009-08-27 17:32:02 +0000

    NOJIRA updated license and added functional test case for REST generate activation keys
    git-svn-id: svn://localhost/openregistry@18479 1580c273-15eb-1042-8a87-dc5d815c88a0

--- trunk/openregistry-api/src/main/java/org/openregistry/core/domain/MockPerson.java
+++ trunk/openregistry-api/src/main/java/org/openregistry/core/domain/MockPerson.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.openregistry.core.service;
+package org.openregistry.core.domain;
 
 import org.openregistry.core.domain.*;
 import org.openregistry.core.domain.sor.SorRole;
---
+++ trunk/openregistry-webapp/src/test/java/org/openregistry/core/web/resources/ActivationKeyProcessorResourceTests.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (C) 2009 Jasig, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openregistry.core.web.resources;
+
+import com.sun.jersey.test.framework.JerseyTest;
+import com.sun.jersey.test.framework.WebAppDescriptor;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.ClientRequest;
+import com.sun.jersey.spi.spring.container.servlet.SpringServlet;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.springframework.web.context.ContextLoaderListener;
+
+/**
+ * @version $Revision$ $Date$
+ * @since 0.1
+ */
+public final class ActivationKeyProcessorResourceTests extends JerseyTest {
+
+    public ActivationKeyProcessorResourceTests() {
+        super(new WebAppDescriptor.Builder("org.openregistry.core.web.resources.activation")
+            .contextPath("openregistry")
+            .contextParam("contextConfigLocation", "classpath:testApplicationContext.xml")
+            .servletClass(SpringServlet.class)
+            .contextListenerClass(ContextLoaderListener.class)
+            .build());      
+    }
+
+    /** Create new activation key. */
+    @Test
+    public void testActivationKeyGeneration() {
+        final WebResource resource = resource().path("people/NetId/testId/activation");
+        final ClientRequest.Builder builder = ClientRequest.create();
+        final ClientRequest request = builder.build(resource.getURI(), "POST");
+
+        final ClientResponse response = client().handle(request);
+        final String locationHeader = response.getHeaders().getFirst("Location");
+        assertNotNull(locationHeader);
+        assertEquals(201, response.getClientResponseStatus().getStatusCode());
+    }
+
+    /** Person Not Found. */
+    @Test
+    public void testPersonNotFound() {
+        final WebResource resource = resource().path("people/Net/testId/activation");
+        final ClientRequest.Builder builder = ClientRequest.create();
+        final ClientRequest request = builder.build(resource.getURI(), "POST");
+
+        final ClientResponse response = client().handle(request);
+        final String locationHeader = response.getHeaders().getFirst("Location");
+        assertNull(locationHeader);
+        assertEquals(404, response.getClientResponseStatus().getStatusCode());
+    }
+}
