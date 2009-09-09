627ebeb5b4050c4538c409f419dac5d2a7cb2646
Nancy Mond <nmond@rutgers.edu>
2009-09-09 16:03:42 +0000

    NOJIRA Mocked classes for unit test.
    git-svn-id: svn://localhost/openregistry@18521 1580c273-15eb-1042-8a87-dc5d815c88a0

---
+++ trunk/openregistry-api/src/main/java/org/openregistry/core/domain/sor/MockSorName.java
@@ -0,0 +1,154 @@
+/**
+ * Copyright (C) 2009 Jasig, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openregistry.core.domain.sor;
+
+import org.openregistry.core.domain.internal.Entity;
+import org.openregistry.core.domain.Name;
+import org.openregistry.core.domain.Person;
+
+/**
+ * Implementation of the Name domain object that conforms to the tables for the Systems of Record
+ *
+ *
+ * @author Nancy Mond
+ * @version $Revision$ $Date$
+ * @since 1.0.0
+ */
+
+public final class MockSorName implements Name {
+
+    private Long id;
+
+    private String prefix;
+
+    private String given;
+
+    private String middle;
+
+    private String family;
+
+    private String suffix;
+
+    private MockSorPerson person;
+
+    public MockSorName() {
+        // nothing to do
+    }
+
+    public MockSorName(final MockSorPerson person) {
+        this.person = person;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getPrefix() {
+        return this.prefix;
+    }
+
+    public String getGiven() {
+        return this.given;
+    }
+
+    public String getMiddle() {
+        return this.middle;
+    }
+
+    public String getFamily() {
+        return this.family;
+    }
+
+    public String getSuffix() {
+        return this.suffix;
+    }
+
+    public void preRemove(){    
+    }
+
+    public void setPrefix(final String prefix) {
+        this.prefix = prefix;
+    }
+
+    public void setGiven(final String given) {
+        this.given = given;
+    }
+
+    public void setMiddle(final String middle) {
+        this.middle = middle;
+    }
+
+    public void setFamily(final String family) {
+        this.family = family;
+    }
+
+    public void setSuffix(final String suffix) {
+        this.suffix = suffix;
+    }
+
+    public String getFormattedName(){
+        final StringBuilder builder = new StringBuilder();
+
+        construct(builder, "", this.family, ",");
+        construct(builder, "", this.given, "");
+
+        return builder.toString();
+    }
+
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+
+        construct(builder, "", this.prefix, " ");
+        construct(builder, "", this.given, " ");
+        construct(builder, "", this.middle, " ");
+        construct(builder, "", this.family, "");
+        construct(builder, ",", this.suffix, "");
+
+        return builder.toString();
+    }
+
+    protected void construct(final StringBuilder builder, final String prefix, final String string, final String delimiter) {
+        if (string != null) {
+            builder.append(prefix);
+            builder.append(string);
+            builder.append(delimiter);
+        }
+    }
+
+	public boolean isOfficialName() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean isPreferredName() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void setOfficialName() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setPreferredName() {
+		// TODO Auto-generated method stub
+
+	}
+
+    public void moveToPerson(MockSorPerson person){
+        this.person = person;
+    }
+}
