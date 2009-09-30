1f54f67f8539c29b05fc6200c46edb93acc19a3c
Dmitriy Kopylenko <dkopylenko@unicon.net>
2009-09-30 15:24:09 +0000

    OR-211: Added tests for DELETE /people/sor/{sorSource}/{sorId}
    git-svn-id: svn://localhost/openregistry@18783 1580c273-15eb-1042-8a87-dc5d815c88a0

---
+++ trunk/openregistry-webapp/src/test/java/org/openregistry/core/web/DeleteSorPersonViaRestResourceTests.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (C) 2009 Jasig, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openregistry.core.web;
+
+import com.sun.jersey.spi.spring.container.servlet.SpringServlet;
+import com.sun.jersey.test.framework.WebAppDescriptor;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+import org.openregistry.core.web.resources.JerseyTestSupport;
+import org.openregistry.core.web.resources.representations.PersonRequestRepresentation;
+import org.springframework.web.context.ContextLoaderListener;
+
+/**
+ * @author Dmitriy Kopylenko
+ * @since 1.0
+ */
+public class DeleteSorPersonViaRestResourceTests extends JerseyTestSupport {
+
+    private static final String EXISTING_SOR_PERSON_TEST_URI = "/people/sor/TEST-SOR/EXISTING-PERSON";
+    private static final String NON_EXISTING_SOR_PERSON_TEST_URI = "/people/sor/TEST-SOR/NON-EXISTING-PERSON";
+
+    public DeleteSorPersonViaRestResourceTests() {
+        super(new WebAppDescriptor.Builder("org.openregistry.core.web.resources")
+                .contextPath("openregistry")
+                .contextParam("contextConfigLocation", "classpath:testDELETE-SORPersonViaRESTApplicationContext.xml")
+                .servletClass(SpringServlet.class)
+                .contextListenerClass(ContextLoaderListener.class)
+                .build());
+    }
+
+    @Test
+    public void deletingNonExistingSorPerson() {
+        assertStatusCodeEqualsForRequestUriAndHttpMethod(404, NON_EXISTING_SOR_PERSON_TEST_URI, "DELETE");
+    }
+
+    @Test
+    public void postAndGetNotAllowedOnSorPersonResource() {
+        assertStatusCodeEqualsForRequestUriAndHttpMethod(405, EXISTING_SOR_PERSON_TEST_URI, "GET");
+        assertStatusCodeEqualsForRequestUriAndHttpMethod(405, EXISTING_SOR_PERSON_TEST_URI, "POST");
+    }
+
+    @Test
+    public void deletingExistingSorPerson() {
+        assertStatusCodeEqualsForRequestUriAndHttpMethod(204, EXISTING_SOR_PERSON_TEST_URI, "DELETE");
+    }
+}
