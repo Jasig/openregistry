a1a83b3d7ad3b19626e03fd05f42cbed24981154
Jeremy Rosenberg <rosey@sfu.ca>
2009-10-16 21:21:10 +0000

    OR-111 Added unit/functional tests to DefaultPersonServiceTests, added checks to verify roles are handled correctly Updated MockSorPerson, MockPerson, MockReferenceRepository to allow for role testing Added MockSorRole and MockRole to facilitate testing
    git-svn-id: svn://localhost/openregistry@18906 1580c273-15eb-1042-8a87-dc5d815c88a0

---
+++ trunk/openregistry-api/src/main/java/org/openregistry/core/domain/MockRole.java
@@ -0,0 +1,175 @@
+package org.openregistry.core.domain;
+
+import org.openregistry.core.domain.internal.*;
+import org.openregistry.core.domain.*;
+import org.openregistry.core.domain.sor.*;
+
+import java.util.*;
+
+/*
+ * Created by IntelliJ IDEA.
+ * User: rosey
+ * Date: Oct 15, 2009
+ * Time: 1:47:09 PM
+ */
+
+public class MockRole extends Entity implements Role{
+
+	private Long id;
+
+	private Type terminationReason;
+
+	private Long sorRoleId;
+
+	public MockRole(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public int getPercentage() {
+		return 0;
+	}
+
+	public void setPercentage(int percentage) {
+	}
+
+	public Type getPersonStatus() {
+		return null;
+	}
+
+	public void setPersonStatus(Type personStatus) {
+	}
+
+	public Sponsor setSponsor() {
+		return null;
+	}
+
+	public Sponsor addSponsor(SorSponsor sorSponsor) {
+		return null;
+	}
+
+	public Sponsor getSponsor() {
+		return null;
+	}
+
+	public Type getTerminationReason() {
+		return terminationReason;
+	}
+
+	public void setTerminationReason(Type reason) {
+		this.terminationReason = reason;
+	}
+
+	public boolean isTerminated() {
+		return false;
+	}
+
+	public void setSorRoleId(Long sorRoleId) {
+		this.sorRoleId = sorRoleId;
+	}
+
+	public Long getSorRoleId() {
+		return sorRoleId;
+	}
+
+	public Set<Address> getAddresses() {
+		return null;
+	}
+
+	public Address addAddress() {
+		return null;
+	}
+
+	public Address addAddress(Address sorAddress) {
+		return null;
+	}
+
+	public Set<Phone> getPhones() {
+		return null;
+	}
+
+	public Set<EmailAddress> getEmailAddresses() {
+		return null;
+	}
+
+	public EmailAddress addEmailAddress() {
+		return null;
+	}
+
+	public EmailAddress addEmailAddress(EmailAddress sorEmailAddress) {
+		return null;
+	}
+
+	public Phone addPhone() {
+		return null;
+	}
+
+	public Phone addPhone(Phone sorPhone) {
+		return null;
+	}
+
+	public Phone removePhoneById(Long id) {
+		return null;
+	}
+
+	public Set<Url> getUrls() {
+		return null;
+	}
+
+	public Url addUrl() {
+		return null;
+	}
+
+	public Url addUrl(Url sorUrl) {
+		return null;
+	}
+
+	public Set<Leave> getLeaves() {
+		return null;
+	}
+
+	public String getTitle() {
+		return null;
+	}
+
+	public OrganizationalUnit getOrganizationalUnit() {
+		return null;
+	}
+
+	public Campus getCampus() {
+		return null;
+	}
+
+	public Type getAffiliationType() {
+		return null;
+	}
+
+	public String getCode() {
+		return null;
+	}
+
+	public String getDisplayableName() {
+		return null;
+	}
+
+	public String getLocalCode() {
+		return null;
+	}
+
+	public void setStart(Date date) {
+	}
+
+	public void setEnd(Date date) {
+	}
+
+	public Date getStart() {
+		return null;
+	}
+
+	public Date getEnd() {
+		return null;
+	}
+}
