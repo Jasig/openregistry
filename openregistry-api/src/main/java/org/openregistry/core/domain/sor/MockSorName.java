c0b1b1abaf8c8d44434115567bf97638ad43fecd
Scott Battaglia <scott.battaglia@gmail.com>
2009-11-06 04:26:11 +0000

    NOJIRA
    added equals and hashcode methods
    
    git-svn-id: svn://localhost/openregistry/trunk@19101 1580c273-15eb-1042-8a87-dc5d815c88a0

--- openregistry-api/src/main/java/org/openregistry/core/domain/sor/MockSorName.java
+++ openregistry-api/src/main/java/org/openregistry/core/domain/sor/MockSorName.java
@@ -149,4 +149,38 @@ public final class MockSorName implements Name {
 	public void setPreferredName(final boolean preferredName) {
         this.preferredName = preferredName;
 	}
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final MockSorName that = (MockSorName) o;
+
+        if (officialName != that.officialName) return false;
+        if (preferredName != that.preferredName) return false;
+        if (family != null ? !family.equals(that.family) : that.family != null) return false;
+        if (given != null ? !given.equals(that.given) : that.given != null) return false;
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (middle != null ? !middle.equals(that.middle) : that.middle != null) return false;
+        if (prefix != null ? !prefix.equals(that.prefix) : that.prefix != null) return false;
+        if (suffix != null ? !suffix.equals(that.suffix) : that.suffix != null) return false;
+        if (type != null ? !type.equals(that.type) : that.type != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (type != null ? type.hashCode() : 0);
+        result = 31 * result + (prefix != null ? prefix.hashCode() : 0);
+        result = 31 * result + (given != null ? given.hashCode() : 0);
+        result = 31 * result + (middle != null ? middle.hashCode() : 0);
+        result = 31 * result + (family != null ? family.hashCode() : 0);
+        result = 31 * result + (suffix != null ? suffix.hashCode() : 0);
+        result = 31 * result + (officialName ? 1 : 0);
+        result = 31 * result + (preferredName ? 1 : 0);
+        return result;
+    }
 }
