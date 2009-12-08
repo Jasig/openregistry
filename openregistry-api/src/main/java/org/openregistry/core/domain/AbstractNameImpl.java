f5f0b91062740e0c9e0178abcb0d35e1d14f1bd8
Scott Battaglia <scott.battaglia@gmail.com>
2009-12-08 16:19:19 +0000

    NOJIRA documentation and license
    git-svn-id: svn://localhost/openregistry/trunk@19489 1580c273-15eb-1042-8a87-dc5d815c88a0

--- openregistry-api/src/main/java/org/openregistry/core/domain/AbstractNameImpl.java
+++ openregistry-api/src/main/java/org/openregistry/core/domain/AbstractNameImpl.java
@@ -1,14 +1,29 @@
+/**
+ * Copyright (C) 2009 Jasig, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openregistry.core.domain;
 
-import org.openregistry.core.domain.internal.Entity;
-
 /**
+ * Provides a common implementation of the sameAs method.
+ *
  * @version $Revision$ $Date$
  * @since 0.1
  */
 public abstract class AbstractNameImpl implements Name {
 
-    public boolean sameAs(final Name name) {
+    public final boolean sameAs(final Name name) {
         if (name == null) {
             return false;
         }
