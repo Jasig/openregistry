ff0009c4c5fd662c43c464768570ae0cf6458732
Scott Battaglia <scott.battaglia@gmail.com>
2009-12-08 14:39:09 +0000

    NOJIRA added private static method to update names
    git-svn-id: svn://localhost/openregistry@19481 1580c273-15eb-1042-8a87-dc5d815c88a0

--- trunk/openregistry-webapp/src/main/java/org/openregistry/core/web/resources/PeopleResourceUtils.java
+++ trunk/openregistry-webapp/src/main/java/org/openregistry/core/web/resources/PeopleResourceUtils.java
@@ -35,28 +35,20 @@ public final class PeopleResourceUtils {
     private PeopleResourceUtils() {
     }
 
-    public static ReconciliationCriteria buildReconciliationCriteriaFrom(final PersonRequestRepresentation request,
-                                                                         ObjectFactory<ReconciliationCriteria> factory,
-                                                                         ReferenceRepository referenceRepository) {
+    public static ReconciliationCriteria buildReconciliationCriteriaFrom(final PersonRequestRepresentation request, final ObjectFactory<ReconciliationCriteria> factory, final ReferenceRepository referenceRepository) {
         final ReconciliationCriteria rc = factory.getObject();
         rc.getSorPerson().setSourceSor(request.systemOfRecordId);
         rc.getSorPerson().setSorId(request.systemOfRecordPersonId);
 
         for (final PersonRequestRepresentation.Name requestName : request.names) {
             final Name name = rc.getSorPerson().addName();
-            name.setGiven(requestName.firstName);
-            name.setFamily(requestName.lastName);
-            name.setMiddle(requestName.middleName);
-            name.setPrefix(requestName.prefix);
-            name.setSuffix(requestName.suffix);
-            Type type = processNameType(referenceRepository, requestName.nameType);
-            name.setType(type);
+            updateName(name, requestName, referenceRepository);
         }
         rc.getSorPerson().setDateOfBirth(request.dateOfBirth);
         rc.getSorPerson().setSsn(request.ssn);
         rc.getSorPerson().setGender(request.gender);
         
-        if (request.reconciliation != null){
+        if (request.reconciliation != null) {
             if (request.reconciliation.address != null){
                 rc.setAddressLine1(request.reconciliation.address.addressLine1);
                 rc.setAddressLine2(request.reconciliation.address.addressLine2);
@@ -64,14 +56,14 @@ public final class PeopleResourceUtils {
                 rc.setRegion(request.reconciliation.address.region);
                 rc.setPostalCode(request.reconciliation.address.postalCode);
             }
-            if (!request.reconciliation.emails.isEmpty()){
+            if (!request.reconciliation.emails.isEmpty()) {
                 rc.setEmailAddress(request.reconciliation.emails.get(0).email);
             }
-            if (!request.reconciliation.phones.isEmpty()){
+            if (!request.reconciliation.phones.isEmpty()) {
                 rc.setPhoneNumber(request.reconciliation.phones.get(0).phoneNumber);
             }
 
-            if (!request.reconciliation.identifiers.isEmpty()){
+            if (!request.reconciliation.identifiers.isEmpty()) {
                 for (final PersonRequestRepresentation.Reconciliation.Identifier identifier : request.reconciliation.identifiers) {
                     final IdentifierType identifierType = referenceRepository.findIdentifierType(identifier.identifierType);
                     Assert.notNull(identifierType);
@@ -82,9 +74,17 @@ public final class PeopleResourceUtils {
         return rc;
     }
 
-    public static void buildModifiedSorPerson(final PersonRequestRepresentation request,
-                                                                         SorPerson sorPerson,
-                                                                         ReferenceRepository referenceRepository) {
+    private static void updateName(final Name name, final PersonRequestRepresentation.Name n, final ReferenceRepository referenceRepository) {
+        name.setFamily(n.lastName);
+        name.setGiven(n.firstName);
+        name.setMiddle(n.middleName);
+        name.setSuffix(n.suffix);
+        name.setPrefix(n.prefix);
+        final Type type = processNameType(referenceRepository, n.nameType);
+        name.setType(type);
+    }
+
+    public static void buildModifiedSorPerson(final PersonRequestRepresentation request, final SorPerson sorPerson, final ReferenceRepository referenceRepository) {
         sorPerson.setDateOfBirth(request.dateOfBirth);
         sorPerson.setSsn(request.ssn);
         sorPerson.setGender(request.gender);
@@ -92,21 +92,17 @@ public final class PeopleResourceUtils {
 
         for (final PersonRequestRepresentation.Name n : request.names) {
             final Name name = sorPerson.addName();
-            name.setFamily(n.lastName);
-            name.setGiven(n.firstName);
-            name.setMiddle(n.middleName);
-            name.setSuffix(n.suffix);
-            name.setPrefix(n.prefix);
-            Type type = processNameType(referenceRepository, n.nameType);
-            name.setType(type);
+            updateName(name, n, referenceRepository);
         }
 
     }
 
-    protected static Type processNameType(ReferenceRepository referenceRepository, String nameType){
+    protected static Type processNameType(final ReferenceRepository referenceRepository, final String nameType) {
         if (nameType != null) {
-            Type type = referenceRepository.findType(Type.DataTypes.NAME, nameType);
-            if (type != null) return type;
+            final Type type = referenceRepository.findType(Type.DataTypes.NAME, nameType);
+            if (type != null) {
+                return type;
+            }
         }
   
         //Simplifying this. Default is Formal.  If more than one name is specified without a type, error will be dealt with
