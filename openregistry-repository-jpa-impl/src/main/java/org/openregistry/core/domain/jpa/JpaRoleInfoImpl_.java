561e95eb221ca3970d9469f18e6c354e5ffdb8ea
Scott Battaglia <scott.battaglia@gmail.com>
2010-04-05 16:54:49 +0000

    NOJIRA
    updated generated meta data
    
    git-svn-id: svn://localhost/openregistry/trunk@20434 1580c273-15eb-1042-8a87-dc5d815c88a0

--- openregistry-repository-jpa-impl/src/main/java/org/openregistry/core/domain/jpa/JpaRoleInfoImpl_.java
+++ openregistry-repository-jpa-impl/src/main/java/org/openregistry/core/domain/jpa/JpaRoleInfoImpl_.java
@@ -2,6 +2,7 @@ package org.openregistry.core.domain.jpa;
 
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
+import org.openregistry.core.domain.jpa.sor.JpaSystemOfRecordImpl;
 
 @StaticMetamodel(JpaRoleInfoImpl.class)
 public abstract class JpaRoleInfoImpl_ {
@@ -11,6 +12,7 @@ public abstract class JpaRoleInfoImpl_ {
 	public static volatile SingularAttribute<JpaRoleInfoImpl, JpaOrganizationalUnitImpl> organizationalUnit;
 	public static volatile SingularAttribute<JpaRoleInfoImpl, JpaCampusImpl> campus;
 	public static volatile SingularAttribute<JpaRoleInfoImpl, JpaTypeImpl> affiliationType;
+	public static volatile SingularAttribute<JpaRoleInfoImpl, JpaSystemOfRecordImpl> systemOfRecord;
 	public static volatile SingularAttribute<JpaRoleInfoImpl, String> code;
 
 }
