47a8ac6846decf7be9c9a6be666a2f56a86b380f
Dmitriy Kopylenko <dkopylenko@unicon.net>
2010-06-29 14:29:07 +0000

    Integration Jersey tests for the AffiliatedEmailResource - WIP...
    git-svn-id: svn://localhost/openregistry/trunk@20922 1580c273-15eb-1042-8a87-dc5d815c88a0

---
+++ openregistry-webapp/src/test/java/org/openregistry/core/web/resources/ProcessAffiliatedEmailResourceTests.java
@@ -0,0 +1,34 @@
+package org.openregistry.core.web.resources;
+
+import com.sun.jersey.spi.spring.container.servlet.SpringServlet;
+import com.sun.jersey.test.framework.WebAppDescriptor;
+import org.junit.Test;
+import org.springframework.web.context.ContextLoaderListener;
+
+import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+
+/**
+ * @since 1.0
+ */
+public class ProcessAffiliatedEmailResourceTests  extends JerseyTestSupport {
+
+    private static final String RESOURCE_UNDER_TEST_URI = "/affiliated-email";
+
+    private static final String WELL_FORMED_EMAIL = "test@email.com";
+
+    public ProcessAffiliatedEmailResourceTests() {
+        super(new WebAppDescriptor.Builder("org.openregistry.core.web.resources")
+                .contextPath("openregistry")
+                .contextParam("contextConfigLocation", "classpath:testPOSTAffiliatedEmailViaRESTApplicationContext.xml")
+                .servletClass(SpringServlet.class)
+                .contextListenerClass(ContextLoaderListener.class)
+                .build());
+    }
+
+    @Test
+    public void incorrectIncomingMediaType() {
+        assertStatusCodeEqualsForRequestUriAndHttpMethodAndMediaTypeAndEntityWithQueryParams(415, RESOURCE_UNDER_TEST_URI,
+                POST_HTTP_METHOD, MediaType.APPLICATION_XML_TYPE, WELL_FORMED_EMAIL, new HashMap<String, String>());
+    }
+}
