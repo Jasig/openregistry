1367cd8ef54fc471f122ec1c764f8bac1934a94a
Lidia Levkovitch <llevkovi@oit.rutgers.edu>
2010-08-11 20:17:05 +0000

    NOJIRA - Initial version of domain objects for disclosure settings
    git-svn-id: svn://localhost/openregistry/trunk@21453 1580c273-15eb-1042-8a87-dc5d815c88a0

---
+++ openregistry-repository-jpa-impl/src/main/java/org/openregistry/core/domain/jpa/JpaDisclosureSettingsImpl_.java
@@ -0,0 +1,17 @@
+package org.openregistry.core.domain.jpa;
+
+import java.util.Date;
+
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@StaticMetamodel(JpaDisclosureSettingsImpl.class)
+public abstract class JpaDisclosureSettingsImpl_ {
+
+	public static volatile SingularAttribute<JpaDisclosureSettingsImpl, Long> id;
+	public static volatile SingularAttribute<JpaDisclosureSettingsImpl, String> disclosureCode;
+	public static volatile SingularAttribute<JpaDisclosureSettingsImpl, Date> lastUpdateDate;
+	public static volatile SingularAttribute<JpaDisclosureSettingsImpl, Boolean> withinGracePeriod;
+	public static volatile SingularAttribute<JpaDisclosureSettingsImpl, JpaPersonImpl> person;
+}
+
