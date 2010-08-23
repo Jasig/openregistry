b600057bc0a3f07b5969dfc7bb22db528ec7d418
Dave Steiner <steiner@rutgers.edu>
2010-08-23 19:13:28 +0000

    NOJIRA Cleanup code
    git-svn-id: svn://localhost/openregistry@21518 1580c273-15eb-1042-8a87-dc5d815c88a0

--- trunk/openregistry-service-impl/src/main/java/org/openregistry/core/service/identifier/AbstractIdentifierAssigner.java
+++ trunk/openregistry-service-impl/src/main/java/org/openregistry/core/service/identifier/AbstractIdentifierAssigner.java
@@ -7,17 +7,20 @@ import org.openregistry.core.domain.Person;
 
 public abstract class AbstractIdentifierAssigner implements IdentifierAssigner {
 
+	/**
+	 * Looks up a personn's identifiers of a given type and returns the first primary, non-deleted one.
+	 * If none found, returns NULL.
+	 */
 	protected final Identifier findPrimaryIdentifier(Person person, String identifierType) {
-		Identifier identifier = null;
 		final Deque<Identifier> identifiersForThisType = person.getIdentifiersByType().get(identifierType);
 		if (identifiersForThisType != null) {
 			for (final Identifier id : identifiersForThisType) {
 				if (id.isPrimary() && ! id.isDeleted()) {
-					identifier = id;
+					return id;
 				}
 			}
 		}
-		return identifier;
+		return null;
 	}
 }
 
