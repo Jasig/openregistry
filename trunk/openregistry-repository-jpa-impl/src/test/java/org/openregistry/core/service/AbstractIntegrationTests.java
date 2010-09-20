1ce4f3701637fdb96f123b8d97f5aa42c3f4d199
Nancy Mond <nmond@rutgers.edu>
2010-09-20 18:46:01 +0000

    NOJIRA
    Added country.
    
    git-svn-id: svn://localhost/openregistry@21658 1580c273-15eb-1042-8a87-dc5d815c88a0

--- trunk/openregistry-repository-jpa-impl/src/test/java/org/openregistry/core/service/AbstractIntegrationTests.java
+++ trunk/openregistry-repository-jpa-impl/src/test/java/org/openregistry/core/service/AbstractIntegrationTests.java
@@ -37,6 +37,7 @@ public abstract class AbstractIntegrationTests extends AbstractTransactionalJUni
         this.simpleJdbcTemplate.update("insert into ctx_data_types (id, data_type, description) values(4, 'TERMINATION', 'FIRED')");
         this.simpleJdbcTemplate.update("insert into ctx_data_types (id, data_type, description) values(5, 'SPONSOR', 'PERSON')");
         this.simpleJdbcTemplate.update("insert into prd_campuses(id, code, name) values(1, 'cam', 'Busch')");
+        this.simpleJdbcTemplate.update("insert into ctd_countries(id, code, name) values(1, 'US', 'USA')");
         this.simpleJdbcTemplate.update("insert into drd_organizational_units(id, campus_id, organizational_unit_t, code, name) values(1, 1, 3, 'cod', 'Department')");
         this.simpleJdbcTemplate.update("insert into prd_system_of_record(id, sor_id) values(1, 'test')");
         this.simpleJdbcTemplate.update("insert into prd_roles(id, title, organizational_unit_id, campus_id, affiliation_t, code, system_of_record_id) values(1, 'Title', 1, 1, 3, 'Code', 1)");
