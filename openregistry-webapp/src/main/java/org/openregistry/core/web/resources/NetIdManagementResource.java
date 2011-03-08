ecc40d72fa4fbedf6baf74104cd3cf1961fe6441
Dmitriy Kopylenko <dkopylenko@unicon.net>
2011-03-08 16:23:55 +0000

    NOJIRA: NetIdManagementResource initial checkin
    git-svn-id: svn://localhost/openregistry/trunk@23116 1580c273-15eb-1042-8a87-dc5d815c88a0

---
+++ openregistry-webapp/src/main/java/org/openregistry/core/web/resources/NetIdManagementResource.java
@@ -0,0 +1,40 @@
+package org.openregistry.core.web.resources;
+
+import org.openregistry.core.service.identifier.NetIdManagementService;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * RESTful resource used to expose functionality of managing the netids of canonical registry Persons
+ *
+ * @since 1.0
+ */
+@Named
+@Singleton
+@Path("/netid")
+public class NetIdManagementResource {
+
+    private static final String PRIMARY_YES_FLAG = "Y";
+
+    private static final String PRIMARY_NO_FLAG = "N";
+
+    private NetIdManagementService netIdManagementService;
+
+    @Inject
+    public NetIdManagementResource(NetIdManagementService netIdManagementService) {
+        this.netIdManagementService = netIdManagementService;
+    }
+
+    @POST
+    @Path("{currentNetId}")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response changeNetId(@FormParam("newNetId") String newNetIdValue,
+                                @DefaultValue("true") @FormParam("primary") boolean primary) {
+        return null;
+    }
+}
