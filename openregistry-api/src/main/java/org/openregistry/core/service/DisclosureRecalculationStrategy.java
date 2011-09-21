6df059b180ac159a1e595b50e78cb81576370996
Lidia Levkovitch <llevkovi@oit.rutgers.edu>
2011-09-21 20:31:53 +0000

    Recalculation strategy for disclosure settings - initial version
    git-svn-id: svn://localhost/openregistry/trunk@25084 1580c273-15eb-1042-8a87-dc5d815c88a0

---
+++ openregistry-api/src/main/java/org/openregistry/core/service/DisclosureRecalculationStrategy.java
@@ -0,0 +1,36 @@
+/**
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.openregistry.core.service;
+
+/**
+ * Determines individual field-level disclosure settings based on disclosure code
+ * @version $Revision$ $Date$
+ */
+public interface DisclosureRecalculationStrategy {
+	
+	public String getName();
+	public String getDescription();
+	
+	public boolean isAddressLinesPublic(String disclosureCode, String addressType, String affiliationType);
+	public boolean isAddressBuildingPublic(String disclosureCode, String addressType, String affiliationType);
+	public boolean isAddressRegionPublic(String disclosureCode, String addressType, String affiliationType);
+	public boolean isEmailPublic(String disclosureCode, String addressType, String affiliationType);
+	public boolean isPhonePublic(String disclosureCode, String addressType, String phoneType, String affiliationType);
+	public boolean isUrlPublic(String disclosureCode, String addressType, String affiliationType);
+}
