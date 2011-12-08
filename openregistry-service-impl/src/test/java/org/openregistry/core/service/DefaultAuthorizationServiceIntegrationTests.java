a4c1c530509a074ed4f4c966255bffab5b62d06b
Naveed Hussain <naveed.hussain@rutgers.edu>
2011-12-08 18:55:19 +0000

    OR-349
    git-svn-id: svn://localhost/openregistry/trunk@25569 1580c273-15eb-1042-8a87-dc5d815c88a0

--- openregistry-service-impl/src/test/java/org/openregistry/core/service/DefaultAuthorizationServiceIntegrationTests.java
+++ openregistry-service-impl/src/test/java/org/openregistry/core/service/DefaultAuthorizationServiceIntegrationTests.java
@@ -33,115 +33,115 @@ import java.util.Set;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:test-authorizationServices-context.xml"})
 public class DefaultAuthorizationServiceIntegrationTests {
-//    @Inject
-//    private AuthorizationService authorizationService;
-//
-//    @PersistenceContext
-//    private EntityManager entityManager;
-//
-//    private User user;
-//    private User user1;
-//    private User user2;
-//    private User user3;
-//
-//    private Group group;
-//    private Group group1;
-//    private Group group2;
-//    private Group group3;
-//
-//    private Authority authority;
-//    private Authority authority1;
-//    private Authority authority2;
-//    private Authority authority3;
-//
-//    @Before
-//    public void dataBaseSetUp() throws Exception {
-//        System.out.println("Before Test Starts: set User Value");
-//
-//        this.user = new JpaUserImpl();
-//        this.user.setUserName("TEST");
-//        this.user.setPassword("TEST");
-//        this.user.setDescription("TEST USER");
-//        this.user.setEnabled(true);
-//
-//        this.user1 = new JpaUserImpl();
-//        this.user1.setUserName("TEST1");
-//        this.user1.setPassword("TEST");
-//        this.user1.setDescription("TEST USER1");
-//        this.user1.setEnabled(true);
-//
-//        this.user2 = new JpaUserImpl();
-//        this.user2.setUserName("TEST2");
-//        this.user2.setPassword("TEST");
-//        this.user2.setDescription("TEST USER2");
-//        this.user2.setEnabled(true);
-//
-//        this.user3 = new JpaUserImpl();
-//        this.user3.setUserName("TEST3");
-//        this.user3.setPassword("TEST");
-//        this.user3.setDescription("TEST USER3");
-//        this.user3.setEnabled(true);
-//
-//        this.group = new JpaGroupImpl();
-//        this.group.setGroupName("Test Group");
-//        this.group.setDescription("Test Group");
-//        this.group.setEnabled(true);
-//
-//        this.group1 = new JpaGroupImpl();
-//        this.group1.setGroupName("Test Group1");
-//        this.group1.setDescription("Test Group1");
-//        this.group1.setEnabled(true);
-//
-//        this.group2 = new JpaGroupImpl();
-//        this.group2.setGroupName("Test Group2");
-//        this.group2.setDescription("Test Group2");
-//        this.group2.setEnabled(true);
-//
-//        this.group3 = new JpaGroupImpl();
-//        this.group3.setGroupName("Test Group3");
-//        this.group3.setDescription("Test Group3");
-//        this.group3.setEnabled(true);
-//
-//
-//        this.authority = new JpaAuthorityImpl();
-//        this.authority.setAuthorityName("TEST_AUTHORITY");
-//        this.authority.setDescription("TEST_AUTH_DESCRIPTION");
-//
-//        this.authority1 = new JpaAuthorityImpl();
-//        this.authority1.setAuthorityName("TEST_AUTHORITY1");
-//        this.authority1.setDescription("TEST_AUTH_DESCRIPTION1");
-//
-//        this.authority2 = new JpaAuthorityImpl();
-//        this.authority2.setAuthorityName("TEST_AUTHORITY2");
-//        this.authority2.setDescription("TEST_AUTH_DESCRIPTION2");
-//
-//        this.authority3 = new JpaAuthorityImpl();
-//        this.authority3.setAuthorityName("TEST_AUTHORITY3");
-//        this.authority3.setDescription("TEST_AUTH_DESCRIPTION3");
-//    }
+    @Inject
+    private AuthorizationService authorizationService;
 
-//    @Test
-//    public void testSaveUser() throws Exception {
-//       //this.authorizationService
-//       User userTest=this.authorizationService.saveUser(this.user);
-//       System.out.println("User Id:" + userTest.getId());
-//    }
-//
-//    @Test
-//    public void testSaveGroup() throws Exception {
-//       //this.authorizationService
-//       Group groupTest =this.authorizationService.saveGroup(this.group);
-//       System.out.println("Group Id:" + groupTest.getId());
-//    }
-//
-//    @Test
-//    public void testSaveAuthority() throws Exception {
-//       //this.authorizationService
-//       Authority testAuthority =this.authorizationService.saveAuthority(this.authority);
-//       System.out.println("Authority Id:" + testAuthority.getId());
-//    }
-//
-//
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private User user;
+    private User user1;
+    private User user2;
+    private User user3;
+
+    private Group group;
+    private Group group1;
+    private Group group2;
+    private Group group3;
+
+    private Authority authority;
+    private Authority authority1;
+    private Authority authority2;
+    private Authority authority3;
+
+    @Before
+    public void dataBaseSetUp() throws Exception {
+        System.out.println("Before Test Starts: set User Value");
+
+        this.user = new JpaUserImpl();
+        this.user.setUserName("TEST");
+        this.user.setPassword("TEST");
+        this.user.setDescription("TEST USER");
+        this.user.setEnabled(true);
+
+        this.user1 = new JpaUserImpl();
+        this.user1.setUserName("TEST1");
+        this.user1.setPassword("TEST");
+        this.user1.setDescription("TEST USER1");
+        this.user1.setEnabled(true);
+
+        this.user2 = new JpaUserImpl();
+        this.user2.setUserName("TEST2");
+        this.user2.setPassword("TEST");
+        this.user2.setDescription("TEST USER2");
+        this.user2.setEnabled(true);
+
+        this.user3 = new JpaUserImpl();
+        this.user3.setUserName("TEST3");
+        this.user3.setPassword("TEST");
+        this.user3.setDescription("TEST USER3");
+        this.user3.setEnabled(true);
+
+        this.group = new JpaGroupImpl();
+        this.group.setGroupName("Test Group");
+        this.group.setDescription("Test Group");
+        this.group.setEnabled(true);
+
+        this.group1 = new JpaGroupImpl();
+        this.group1.setGroupName("Test Group1");
+        this.group1.setDescription("Test Group1");
+        this.group1.setEnabled(true);
+
+        this.group2 = new JpaGroupImpl();
+        this.group2.setGroupName("Test Group2");
+        this.group2.setDescription("Test Group2");
+        this.group2.setEnabled(true);
+
+        this.group3 = new JpaGroupImpl();
+        this.group3.setGroupName("Test Group3");
+        this.group3.setDescription("Test Group3");
+        this.group3.setEnabled(true);
+
+
+        this.authority = new JpaAuthorityImpl();
+        this.authority.setAuthorityName("TEST_AUTHORITY");
+        this.authority.setDescription("TEST_AUTH_DESCRIPTION");
+
+        this.authority1 = new JpaAuthorityImpl();
+        this.authority1.setAuthorityName("TEST_AUTHORITY1");
+        this.authority1.setDescription("TEST_AUTH_DESCRIPTION1");
+
+        this.authority2 = new JpaAuthorityImpl();
+        this.authority2.setAuthorityName("TEST_AUTHORITY2");
+        this.authority2.setDescription("TEST_AUTH_DESCRIPTION2");
+
+        this.authority3 = new JpaAuthorityImpl();
+        this.authority3.setAuthorityName("TEST_AUTHORITY3");
+        this.authority3.setDescription("TEST_AUTH_DESCRIPTION3");
+    }
+
+    @Test
+    public void testSaveUser() throws Exception {
+       //this.authorizationService
+       User userTest=this.authorizationService.saveUser(this.user);
+       System.out.println("User Id:" + userTest.getId());
+    }
+
+    @Test
+    public void testSaveGroup() throws Exception {
+       //this.authorizationService
+       Group groupTest =this.authorizationService.saveGroup(this.group);
+       System.out.println("Group Id:" + groupTest.getId());
+    }
+
+    @Test
+    public void testSaveAuthority() throws Exception {
+       //this.authorizationService
+       Authority testAuthority =this.authorizationService.saveAuthority(this.authority);
+       System.out.println("Authority Id:" + testAuthority.getId());
+    }
+
+
 //    @Test
 //    public void testFindAllUsers() throws Exception {
 //       //this.authorizationService
@@ -164,24 +164,24 @@ public class DefaultAuthorizationServiceIntegrationTests {
 //           System.out.println("[USER] enabled: " + user1.isEnabled());
 //       }
 //    }
-//
-//    @Test
-//    public void testFindAllGroups() throws Exception {
-//       //this.authorizationService
-//        this.authorizationService.saveGroup(this.group);
-//        this.authorizationService.saveGroup(this.group1);
-//        this.authorizationService.saveGroup(this.group2);
-//        this.authorizationService.saveGroup(this.group3);
-//
-//       List<Group> groups= this.authorizationService.findAllGroups();
-//       for (Group group1: groups){
-//           System.out.println("[Group] id:" + group1.getId());
-//           System.out.println("[Group] name:" + group1.getGroupName());
-//           System.out.println("[Group] description:" + group1.getDescription());
-//           System.out.println("[Group] enabled: " + group1.isEnabled());
-//       }
-//    }
-//
+
+    @Test
+    public void testFindAllGroups() throws Exception {
+       //this.authorizationService
+        this.authorizationService.saveGroup(this.group);
+        this.authorizationService.saveGroup(this.group1);
+        this.authorizationService.saveGroup(this.group2);
+        this.authorizationService.saveGroup(this.group3);
+
+       List<Group> groups= this.authorizationService.findAllGroups();
+       for (Group group1: groups){
+           System.out.println("[Group] id:" + group1.getId());
+           System.out.println("[Group] name:" + group1.getGroupName());
+           System.out.println("[Group] description:" + group1.getDescription());
+           System.out.println("[Group] enabled: " + group1.isEnabled());
+       }
+    }
+
 //    @Test
 //    public void testFindAllAuthorities() throws Exception {
 //       //this.authorizationService
@@ -197,7 +197,7 @@ public class DefaultAuthorizationServiceIntegrationTests {
 //           System.out.println("[Authority] description:" + authority1.getDescription());
 //       }
 //    }
-//
+
 //    @Test
 //    public void testSearchUserByName() throws Exception{
 //        System.out.println("####################################TEST USER######################################");
@@ -217,7 +217,7 @@ public class DefaultAuthorizationServiceIntegrationTests {
 //           System.out.println("[USER] enabled: " + user1.isEnabled());
 //       }
 //    }
-//
+
 //    @Test
 //    public void testSearchGroupByName() throws Exception{
 //        System.out.println("####################################TEST GROUP######################################");
@@ -234,7 +234,7 @@ public class DefaultAuthorizationServiceIntegrationTests {
 //           System.out.println("[Group] enabled: " + group1.isEnabled());
 //       }
 //    }
-//
+
 //    @Test
 //    public void testSearchAuthorityByName() throws Exception{
 //        System.out.println("####################################TEST AUTHORITY######################################");
@@ -252,7 +252,7 @@ public class DefaultAuthorizationServiceIntegrationTests {
 //
 //    }
 
-//
+
 //     @Test
 //    public void testUserGroupAuthorityRelationship() throws Exception {
 //
